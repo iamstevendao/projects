1083d7c95b6ad368b81478069809febe9b9b0344
Steven Dao <iamstevendao@gmail.com>
2018-10-10 23:08:13 +1100

    Avoid committing without changes

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 
 git add -A
-git commit -m "[CI cron] Update sub-modules && deploy"
+git diff-index --quiet HEAD || git commit -m "[CI cron] Update sub-modules && deploy"
 
 git push -f git@github.com:iamstevendao/projects.git master:master
 
